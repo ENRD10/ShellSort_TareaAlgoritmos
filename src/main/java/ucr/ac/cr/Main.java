1a53ef8ee1ace60141c6b35aa1b6e7c0d187e96b
elcorderodedios <SEBASTIAN.CORDEROQUESADA@ucr.ac.cr>
2024-05-16 01:08:34 -0600

    Update Main.java

--- src/main/java/ucr/ac/cr/Main.java
+++ src/main/java/ucr/ac/cr/Main.java
@@ -1,17 +1,35 @@
 package ucr.ac.cr;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.List;
+import java.util.Comparator;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class ShellSort<T> {
+
+    // Método para ordenar una lista utilizando el algoritmo Shell Sort
+    public void shellSort(List<T> lista, Comparator<T> comparator) {
+        int salto, i; // Variables para controlar el tamaño de los saltos y las iteraciones
+        T aux; //para intercambiar elementos
+        boolean cambios; // Variable para controlar si se realizan cambios en la lista
+        int cont = lista.size(); //longitud
+
+        // reduce el tamaño de los saltos a la mitad en cada iteración
+        for (salto = cont / 2; salto != 0; salto /= 2) {
+            cambios = true; // Inicializar cambios como verdadero al inicio de cada iteración
+            //  mientras haya cambios en la lista
+            while (cambios) {
+                cambios = false; // Inicializar cambios como falso al inicio de cada iteración
+                // Bucle que recorre la lista y realiza comparaciones entre elementos
+                for (i = salto; i < cont; i++) {
+                    // Si el elemento en la posición actual es mayor que el elemento en la posición actual - salto,
+                    // se intercambian los elementos y se marca cambios como verdadero
+                    if (comparator.compare(lista.get(i - salto), lista.get(i)) < 0) {
+                        aux = lista.get(i); // Almacenar el elemento en la posición actual en la variable auxiliar
+                        lista.set(i, lista.get(i - salto)); // Establecer el elemento en la posición actual - salto en la posición actual
+                        lista.set(i - salto, aux); // Establecer el elemento almacenado en la variable auxiliar en la posición actual - salto
+                        cambios = true; // Marcar cambios como verdadero
+                    }
+                }
+            }
         }
     }
 }
